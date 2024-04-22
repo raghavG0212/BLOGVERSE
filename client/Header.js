12c52451aaa35f46a76afbf15ed652e4faf89454
raghavG0212 <147530496+raghavG0212@users.noreply.github.com>
2024-04-22 10:18:54 +0530

    frontend

---
+++ client/Header.js
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "./UserContext";
+
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+
+  useEffect(() => {
+    // Fetch user profile
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    })
+      .then(response => response.json())
+      .then(userInfo => {
+        setUserInfo(userInfo);
+      })
+      .catch(error => console.error('Error fetching user profile:', error));
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">BlogVerse</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
+
+
